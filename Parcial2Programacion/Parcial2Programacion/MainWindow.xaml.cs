8d22101294c19e7c5abde34d12bc10e90ef7df8a
Christopher414704 <antoniocristofer577@gmail.com>
2024-04-27 10:58:56 -0600

    Parcial

---
+++ Parcial2Programacion/Parcial2Programacion/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Parcial2Programacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter escribir = new StreamWriter("");
+
+            try
+            {
+                escribir.WriteLine();
+                escribir.WriteLine();
+                escribir.WriteLine();
+                escribir.WriteLine();
+            }
+        }
+    }
+}
